33baf994fb0e6628ab7e23b81d4434eb4111b4d5
pikkilisaikumar <83571587+pikkilisaikumar@users.noreply.github.com>
2022-11-07 21:46:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-wbciuq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-wbciuq)
---
+++ index.js
@@ -0,0 +1,5 @@
+let array = [5, 3, 2, 8, 9, 7];
+
+let filter_odd = array.filter((each) => each % 2 !== 0);
+
+console.log(filter_odd);
